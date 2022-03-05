e3fb067f9ee8f7b702e1cc7dc04eaf35ad6b3b88
Angel Soto <angells825@gmail.com>
2022-03-04 16:19:41 -0800

    search method added to tree

--- tree_ex.py
+++ tree_ex.py
@@ -4,10 +4,27 @@ class Node:
             self.left = None
             self.right = None
             
+        def search(self, target):
+            if self.data == target:
+                print("Found it!")
+                return self
+            
+            if self.left and self.data > target:
+                return self.left.search(target)
+            
+            if self.right and self.data < target:
+                return self.right.search(target)
+            
+            print("value is not it tree")
+            
 class Tree:
     def __init__(self, root, name=''):
         self.root = root
         self.name = name
+    
+    def search(self, target):
+        return self.root.search(target)
+        
 
 node = Node(10)
 
@@ -22,7 +39,10 @@ node.right.right = Node(100)
 
 myTree = Tree(node, "a tree")
 
-print(myTree.name)
-print(myTree.root.left.data)
-print(myTree.root.right.right.data)
+##print(myTree.name)
+##print(myTree.root.left.data)
+##print(myTree.root.right.right.data)
+
+found = myTree.search(100)
+print(found.data)
     
