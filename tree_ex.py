0744c03cee7923d2b8cc4797c9261eae7bc07c2b
Angel Soto <angells825@gmail.com>
2022-03-07 14:23:46 -0800

    Order Traversals

--- tree_ex.py
+++ tree_ex.py
@@ -16,6 +16,27 @@ class Node:
                 return self.right.search(target)
             
             print("value is not it tree")
+        
+        def traversePreorder(self):
+            print(self.data)
+            if self.left:
+                self.traversePreorder() 
+            if self.right:
+                self.right.traverPreorder()
+            
+        def traverseInorder(self):
+            if self.left:
+                self.left.traverseInroder()
+            print(self.data)
+            if self.right:
+                self.right.traverseInroder()
+            
+        def traversePostorder(self):
+            if self.left:
+                self.left.traversePostorder()
+            if self.right:
+                self.right.traversePostorder()
+            print(self.data)
             
 class Tree:
     def __init__(self, root, name=''):
@@ -24,25 +45,48 @@ class Tree:
     
     def search(self, target):
         return self.root.search(target)
+    
+    def traversePreorder(self):
+        self.root.traversePreorder()
         
+    def traverseInorder(self):
+        self.root.traverseInorder()
+        
+    def traversePostorder(self):
+        self.root.traversePostorder()
+        
+myTree = Tree(Node(50), 'Tree traversals')
+myTree.root.left = Node(25)
+myTree.root.right = Node(75)
+myTree.root.left.left = Node(10)
+myTree.root.left.right = Node(35)
+myTree.root.left.right.left = Node(30)
+myTree.root.left.right.right = Node(42)
+myTree.root.left.left.left = Node(5)
+myTree.root.left.left.right = Node(13)
 
-node = Node(10)
+print("Traverse Pre-Order")
+myTree.traversePreorder()
 
-node.left = Node(5)
-node.right = Node(15)
+print("\nTraverse In-Order")
+myTree.traverseInorder()
 
-node.left.left = Node (2)
-node.left.right = Node(6)
+print("\nTraverse Post-Order")
+myTree.traversePostorder()
 
-node.right.left = Node(13)
-node.right.right = Node(100)
 
-myTree = Tree(node, "a tree")
+## previous search value in tree build
 
+##node = Node(10)
+##node.right = Node(15)
+##node.left.left = Node (2)
+##node.left.right = Node(6)
+##node.right.left = Node(13)
+##node.right.right = Node(100)
+##myTree = Tree(node, "a tree")
 ##print(myTree.name)
 ##print(myTree.root.left.data)
 ##print(myTree.root.right.right.data)
-
-found = myTree.search(100)
-print(found.data)
+##found = myTree.search(100)
+##print(found.data)
     
